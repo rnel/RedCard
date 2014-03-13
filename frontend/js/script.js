d81c607cda13669a8a78099f3109c8b2e9e8d430
Arnel Aguinaldo <arnel.aguinaldo@rga.com>
2014-03-13 16:25:41 +0800

    added frontend

---
+++ frontend/js/script.js
@@ -0,0 +1,66 @@
+
+$(document).ready(function(){
+
+  // Masonry
+  var container = $('#persons-container');
+  var msnry = new Masonry( container[0], {
+    itemSelector: '.person-item'
+  });
+
+  function addNewPerson(data) {
+    var len = container.find('.person-item').length;
+
+    if (len < 3) {
+      container.removeClass();
+      container.addClass('contains-'+(len+1));
+    }
+
+    var element = '<div id="' + data.id + '" class="person-item">\
+                    <img src="' + data.url + '">\
+                    <p>' + data.first_name + ' ' + data.last_name + '</p>\
+                  </div>';
+
+    var newPersonEl = $(element).prependTo(container);
+    msnry.prepended(newPersonEl);
+  }
+
+  function removePerson(id) {
+    msnry.remove( $('#'+id) );
+    msnry.layout();
+  }
+
+
+  // Socket IO
+  var socket = io.connect('http://localhost:1337/', {'force new connection': true});
+
+  socket.on('connect', function(){
+    console.log('connected');
+  });
+
+  socket.on('disconnect', function(){
+    console.log('disconected');
+  });
+
+  socket.on('add person', function (data){
+    console.log('data', data);
+    addNewPerson(data);
+  });
+
+  socket.on('remove person', function (id){
+    console.log('id', id);
+    removePerson(id);
+  });
+
+  //test
+  window.a = function() {
+    var d = {
+      "id": "1234567",
+      "first_name": "Lorem",
+      "last_name": "Ipsum",
+      "url": "http://www.faithlineprotestants.org/wp-content/uploads/2010/12/facebook-default-no-profile-pic.jpg"
+    }
+
+    addNewPerson(d);
+  }
+
+});
